ab6fc05862d8216d5da7dd9f648bc88ff27a35c3
wonleeeee <won8303@gmail.com>
2022-01-01 23:34:46 -0800

    220102

--- twoSum.c
+++ twoSum.c
@@ -37,11 +37,11 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     // Search through an input array
     for (i=0; i<numsSize; i++) {
         
-        index = hash(nums[i], numsSize);
+        index = hash(target-nums[i], numsSize);
         tmp = hash_table[index];
 
         // Look up the hash table
-        while (tmp!=NULL && tmp->num==(target-nums[i])) {
+        while (tmp!=NULL && tmp->num!=(target-nums[i])) {
             tmp = tmp->next;
         }
 
@@ -49,13 +49,22 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
         if (tmp != NULL) {
             returnVal[0] = tmp->pos;
             returnVal[1] = i;
+
+            // Free malloced hash table
+            for (i=0; i<numsSize; i++) {
+                tmp = hash_table[i];
+                while (tmp!=NULL) {
+                    hash_table[i] = tmp;
+                    tmp = tmp->next;
+                    free(hash_table[i]);
+                }
+            }
             return returnVal;
         }
 
         // Add to the hash table, if it does not exists
         index = hash(nums[i], numsSize);
         newNode = malloc(sizeof(info));
-        // newNode = {.num=nums[i], .pos=i, .next=hash_table[index]};
         newNode->num = nums[i];
         newNode->pos = i;
         newNode->next = hash_table[index];
@@ -67,15 +76,39 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 
 int main() {
     int numsSize, target, *returnArray, returnSize;
-    int nums[] = {2, 7, 11, 15};
+    int nums1[] = {2, 7, 11, 15};
+    int nums2[] = {3,2,4};
+    int nums3[] = {3,3};
 
     numsSize = 4;
     target = 9;
     returnSize = 2;
 
-    returnArray = twoSum(nums, numsSize, target, &returnSize);
+    returnArray = twoSum(nums1, numsSize, target, &returnSize);
 
-    printf("nums : [2, 7, 11, 15],\ntarget : %d -> %i, %i\n", target, returnArray[0], returnArray[1]);
+    printf("nums1 : [2, 7, 11, 15],\ntarget : %d -> %i, %i\n", target, returnArray[0], returnArray[1]);
+
+    free(returnArray);
+
+    numsSize = 3;
+    target = 6;
+    returnSize = 2;
+
+    returnArray = twoSum(nums2, numsSize, target, &returnSize);
+
+    printf("nums2 : [3,2,4],\ntarget : %d -> %i, %i\n", target, returnArray[0], returnArray[1]);
+
+    free(returnArray);
+
+    numsSize = 2;
+    target = 6;
+    returnSize = 2;
+
+    returnArray = twoSum(nums3, numsSize, target, &returnSize);
+
+    printf("nums3 : [3, 3],\ntarget : %d -> %i, %i\n", target, returnArray[0], returnArray[1]);
+
+    free(returnArray);
 
     return 0;
 }
