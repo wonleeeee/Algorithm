02ac0e4728447d34b2522825035cb107b01dcbeb
wonleeeee <won8303@gmail.com>
2022-01-02 00:03:44 -0800

    220102

--- BestTimeToBuy.c
+++ BestTimeToBuy.c
@@ -5,24 +5,42 @@
 #include <stdbool.h>
 
 int maxProfit(int* prices, int pricesSize) {
-    uint16_t i;
-    uint32_t best[pricesSize];
+    int i, start, end, max, curMax;
+
+    start = 0;
+    end = 0;
+    max = 0;
+    curMax = 0;
 
     for (i=0; i<pricesSize; i++) {
-        
+        if (prices[start] > prices[i]) {
+            start = i;
+            end = i;
+        }
+        else if (prices[end] < prices[i]) {
+            end = i;
+            curMax = prices[end] - prices[start];
+            if (max < curMax) {
+                max = curMax;
+            }
+        }
     }
 
-    return 0;
+    return max;
 }
 
 int main() {
-    int prices[] = {7, 1, 5, 3, 6, 4};
-    int pricesSize = 6;
+    int prices1[] = {7, 1, 5, 3, 6, 4};
+    int prices2[] = {7, 6, 4, 3, 1};
+    int pricesSize1 = 6;
+    int pricesSize2 = 5;
     int result;
 
-    result = maxProfit(prices, pricesSize);
+    result = maxProfit(prices1, pricesSize1);
+    printf("[7, 1, 5, 3, 6, 4] : %i\n", result);
 
-    printf("%i", result);
+    result = maxProfit(prices2, pricesSize2);
+    printf("[7, 6, 4, 3, 1] : %i\n", result);
 
     return 0;
 }
